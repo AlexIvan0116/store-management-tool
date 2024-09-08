c9039430ffb982a7f265a6a24a6f83e1bf869fb4
AlexIvan0116 <alexandru.ivan11@gmail.com>
2024-09-08 12:28:34 +0300

    get all products test

--- src/test/java/com/project/store_management_tool/controller/ProductControllerTest.java
+++ src/test/java/com/project/store_management_tool/controller/ProductControllerTest.java
@@ -1,13 +1,27 @@
 package com.project.store_management_tool.controller;
 
+import com.project.store_management_tool.model.Product;
 import com.project.store_management_tool.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductControllerTest {
@@ -22,5 +36,21 @@ public class ProductControllerTest {
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void getAllProducts() throws Exception {
+        List<Product> productList = getProducts();
+        Mockito.when(productService.getAll()).thenReturn(productList);
+
+        mockMvc.perform(get("/api/product/get/all")
+                .header("Authorization", "Bearer token").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[2]").exists());
+    }
+
+    private List<Product> getProducts() {
+        return Arrays.asList(new Product(), new Product(), new Product());
     }
 }
