71662fe07d845651db9053e6ad37893c3a0a2da5
AlexIvan0116 <alexandru.ivan11@gmail.com>
2024-09-08 12:13:05 +0300

    product controller unit test - config

---
+++ src/test/java/com/project/store_management_tool/controller/ProductControllerTest.java
@@ -0,0 +1,26 @@
+package com.project.store_management_tool.controller;
+
+import com.project.store_management_tool.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductControllerTest {
+    @Mock
+    private ProductService productService;
+
+    @InjectMocks
+    private ProductController productController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
+}
